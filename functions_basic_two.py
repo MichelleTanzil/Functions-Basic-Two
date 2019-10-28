835237f7b43c330c4449716777f063dfd01dba2b
Michelle Tanzil <mtanzil01@gmail.com>
2019-10-28 15:05:52 -0700

    changed argument names from list to li

--- functions_basic_two.py
+++ functions_basic_two.py
@@ -15,24 +15,24 @@ print_and_return([1,2])
 # Example: print_and_return([1,2]) should print 1 and return 2
 
 # First Plus Length - Create a function that accepts a list and returns the sum of the first value in the list plus the list's length.
-def first_plus_length(list):
-    return list[0] + len(list)
+def first_plus_length(li):
+    return li[0] + len(li)
 first_plus_length([1,2,3,4,5])
 # Example: first_plus_length([1,2,3,4,5]) should return 6 (first value: 1 + length: 5)
 
 # Values Greater than Second - Write a function that accepts a list and creates a new list containing only the values from the original list that are greater than its 2nd value. Print how many values this is and then return the new list. If the list has less than 2 elements, have the function return False
-def values_greater_than_second(list):
-    list_second = []    
+def values_greater_than_second(li):
+    li_second = []    
     count = 0
     if len(list) < 2:
         return False
     else:
-        for x in range(0, len(list)):
-            if list[x] > list[1]:
+        for x in range(0, len(li)):
+            if li[x] > li[1]:
                 count += 1
-                list_second.append(list[x])
+                li_second.append(li[x])
         print(count)
-        return list_second
+        return li_second
 values_greater_than_second([5,2,3,2,1,4])
 values_greater_than_second([3])
 # Example: values_greater_than_second([5,2,3,2,1,4]) should print 3 and return [5,3,4]
